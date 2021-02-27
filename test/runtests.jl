12dea99a2407ef9b4c3a301070cbfac57db591da
David Anthoff <anthoff@berkeley.edu>
2021-02-26 21:47:14 -0800

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,44 +3,56 @@ using Test
 
 @testset "MPSGE" begin
 
-    m = Model()
+    @testset "TWOBYTWO" begin
+        m = Model()
 
-    add!(m, Sector(:X))
-    add!(m, Sector(:Y))
-    add!(m, Sector(:U))
+        add!(m, Sector(:X))
+        add!(m, Sector(:Y))
+        add!(m, Sector(:U))
 
-    add!(m, Commodity(:PX))
-    add!(m, Commodity(:PY))
-    add!(m, Commodity(:PU))
-    add!(m, Commodity(:PL))
-    add!(m, Commodity(:PK))
+        add!(m, Commodity(:PX))
+        add!(m, Commodity(:PY))
+        add!(m, Commodity(:PU))
+        add!(m, Commodity(:PL))
+        add!(m, Commodity(:PK))
 
-    add!(m, Consumer(:RA, benchmark=150.))
+        add!(m, Consumer(:RA, benchmark=150.))
 
-    add!(m, Production(:X, 1, :PX, 100, [Input(:PL, 50), Input(:PK, 50)]))
-    add!(m, Production(:Y, 1, :PY, 50, [Input(:PL, 20), Input(:PK, 30)]))
-    add!(m, Production(:U, 1, :PU, 150, [Input(:PX, 100), Input(:PY, 50)]))
+        add!(m, Production(:X, 1, :PX, 100, [Input(:PL, 50), Input(:PK, 50)]))
+        add!(m, Production(:Y, 1, :PY, 50, [Input(:PL, 20), Input(:PK, 30)]))
+        add!(m, Production(:U, 1, :PU, 150, [Input(:PX, 100), Input(:PY, 50)]))
 
-    add!(m, Demand(:RA, :PU, [Endowment(:PL, 70), Endowment(:PK, 80)]))
-    
-    solve!(m)
+        add!(m, Demand(:RA, :PU, [Endowment(:PL, 70), Endowment(:PK, 80)]))
 
-    @test value(m, :X) ≈ 1.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:Y]) ≈ 1.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:U]) ≈ 1.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:RA]) ≈ 150.
+        solve!(m)
 
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PX]) ≈ 1.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PY]) ≈ 1.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PU]) ≈ 1.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PL]) ≈ 1.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PK]) ≈ 1.
+        @test value(m, :X) ≈ 1.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:Y]) ≈ 1.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:U]) ≈ 1.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:RA]) ≈ 150.
 
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PLPX]) ≈ 50.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PLPY]) ≈ 20.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PKPX]) ≈ 50.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PKPY]) ≈ 30.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PXPU]) ≈ 100.
-    @test MPSGE.Complementarity.result_value(m._jump_model[:PYPU]) ≈ 50.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PX]) ≈ 1.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PY]) ≈ 1.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PU]) ≈ 1.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PL]) ≈ 1.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PK]) ≈ 1.
+
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PLPX]) ≈ 50.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PLPY]) ≈ 20.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PKPX]) ≈ 50.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PKPY]) ≈ 30.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PXPU]) ≈ 100.
+        @test MPSGE.Complementarity.result_value(m._jump_model[:PYPU]) ≈ 50.
+
+        avm = algebraic_version(m)
+
+        @test typeof(avm) == MPSGE.AlgebraicWrapper
+
+        # For now just run these functions, we might add tests for the results
+        # at a later point
+        repr(MIME("text/plain"), m)
+        repr(MIME("text/plain"), avm)
+        repr(MIME("text/latex"), avm)
+    end
     
 end
