533c0a559c08f728288c3086754d8a7c48bc5e7d
David Anthoff <anthoff@berkeley.edu>
2021-05-13 11:23:46 -0700

    Change example 3 to target form

--- examples/example3.jl
+++ examples/example3.jl
@@ -2,26 +2,25 @@ using MPSGE
 
 m = Model()
 
-sectors = [:x, :y]
+i = [:x, :y]
+f = [:l, :k]
 
 @parameter(m, endow, 1.0)
 
-S = add!(m, Sector(:S, indices=(sectors,)))
-@sector(m, U)
+Y = add!(m, Sector(:Y, indices=(i,)))
+U = add!(m, Sector(:U))
 
-@commodity(m, PX)
-@commodity(m, PY)
-@commodity(m, PU)
-@commodity(m, PL)
-@commodity(m, PK)
+PC = add!(m, Commodity(:PC, indices=(i,)))
+PU = add!(m, Commodity(:PU))
+PF = add!(m, Commodity(:PF, indices=(f,)))
 
-@consumer(m, RA, benchmark=150.)
+RA = add!(m, Consumer(:RA, benchmark=150.))
 
-@production(m, S[:x], 1, PX, 100, [Input(PL, 50), Input(PK, 50)])
-@production(m, S[:y], 1, PY, 50, [Input(PL, 20), Input(PK, 30)])
-@production(m, U, 1, PU, 150, [Input(PX, 100), Input(PY, 50)])
+@production(m, Y[:x], 1, PC[:x], 100, [Input(PF[:l], 50), Input(PF[:k], 50)])
+@production(m, Y[:y], 1, PC[:y], 50, [Input(PF[:l], 20), Input(PF[:k], 30)])
+@production(m, U, 1, PU, 150, [Input(PC[:x], 100), Input(PC[:y], 50)])
 
-@demand(m, RA, PU, [Endowment(PL, :(70 * $endow)), Endowment(PK, 80.)])
+@demand(m, RA, PU, [Endowment(PF[:l], :(70 * $endow)), Endowment(PF[:k], 80.)])
 
 solve!(m, cumulative_iteration_limit=0)
 
