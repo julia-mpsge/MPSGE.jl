bac2490c3b7b61c5d961c5125c42adb8890779cc
EliLazarus <lazarus.eli@gmail.com>
2022-07-24 13:26:38 -0700

    Adjust ex3 to callibrate with .2 tax

--- examples/example3.jl
+++ examples/example3.jl
@@ -6,7 +6,11 @@ m = Model()
 goods = [:x, :y]
 factors = [:l, :k]
 
-factor = DenseAxisArray(Float64[50 50; 20 30], goods, factors)
+outtax = add!(m, Parameter(:outtax, value = 0.2))
+
+factor = DenseAxisArray(Float64[30 50; 20 20], goods, factors)
+
+# factor = DenseAxisArray(Float64[:(50 - 100 * $(outtax)) 50; 20 :(30-50*$(outtax))], goods, factors)
 supply = DenseAxisArray(Float64[100, 50], goods)
 
 endow = add!(m, Parameter(:endow, indices=(factors,), value=1.0))
@@ -18,16 +22,17 @@ PC = add!(m, Commodity(:PC, indices=(goods,)))
 PU = add!(m, Commodity(:PU))
 PF = add!(m, Commodity(:PF, indices=(factors,)))
 
-RA = add!(m, Consumer(:RA, benchmark=150.))
+RA = add!(m, Consumer(:RA, benchmark=120.))
 
 for i in goods
-    @production(m, Y[i], 0, 1, [Output(PC[i], supply[i], [MPSGE.Tax(0.5, RA)])], [Input(PF[:l], factor[i,:l]), Input(PF[:k], factor[i,:k])])
+    @production(m, Y[i], 0, 1, [Output(PC[i], supply[i], [MPSGE.Tax(0.2, RA)])], [Input(PF[:l], factor[i,:l]), Input(PF[:k], factor[i,:k])])
 end
 # @production(m, [i in goods], Y[i], 1, PC[i], supply[i],  [Input(PF[f], factor[i,f]) for f in factors])
 
 @production(m, U,0, 1, [Output(PU, 150)], [Input(PC[:x], 100), Input(PC[:y], 50)])
 
-@demand(m, RA, 1., [Demand(PU, 150)], [Endowment(PF[:l], :(70 * $(endow[:l]))), Endowment(PF[:k], :(80. * $(endow[:k])))])
+@demand(m, RA, 1., [Demand(PU, 150)], [Endowment(PF[:l], :(50 * $(endow[:l]))), Endowment(PF[:k], :(70 * $(endow[:k])))])
+# @demand(m, RA, 1., [Demand(PU, 150)], [Endowment(PF[:l], :((70-100*$(outtax)) * $(endow[:l]))), Endowment(PF[:k], :((80.0 -50*$(outtax)) * $(endow[:k])))])
 
 set_fixed!(PC[:x], true) # Set sector x as the numeraire
 solve!(m, cumulative_iteration_limit=0)
@@ -38,7 +43,8 @@ algebraic_version(m)
 set_fixed!(PC[:x], false) # unfix, seems to be automatic with new numeraire in MPSGE
 set_value(endow[:l], 1.1.*get_value(endow[:l]))
 # In the original indexed, endow[:l] = 1.1*endow[:l]
-set_value(RA, get_value(endow[:k]) * 80. + get_value(endow[:l]) * 70.)
+# set_value(RA, get_value(endow[:k]) * 80. + get_value(endow[:l]) * 70.)
+set_value(RA, 155.)
 set_fixed!(RA, true)
 solve!(m)
 
