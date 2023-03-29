b8ff5a9d5375bfc9761ffcdfabc40832e34e473c
EliLazarus <lazarus.eli@gmail.com>
2023-03-28 20:46:35 -0700

    update interp in get_tax_rev

--- src/build/build_helpers.jl
+++ src/build/build_helpers.jl
@@ -178,11 +178,11 @@ function get_tax_revenue_for_consumer(jm, m, cr::ConsumerRef)
             for tax in output.taxes
                 if cr.subindex === nothing
                     if get_full(tax.agent) == get_full(cr)    
-                        push!(taxes, :($(tax.rate) * $jm[get_comp_supply_name($output)] * $(output.commodity) * $(pf.sector)))
+                        push!(taxes, :($(tax.rate) * $(jm[get_comp_supply_name(output)]) * $(output.commodity) * $(pf.sector)))
                     end
                 else
                     if jm[get_full(cr).name][tax.agent.subindex] ==  jm[get_full(cr).name][cr.subindex]
-                        push!(taxes, :($(tax.rate) * $jm[get_comp_supply_name($output)] * $(output.commodity) * $(pf.sector)))
+                        push!(taxes, :($(tax.rate) * $(jm[get_comp_supply_name(output)]) * $(output.commodity) * $(pf.sector)))
                     end
                 end    
             end
