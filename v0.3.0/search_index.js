feb207edf15fc13b123d402ee4b206f72a3c0782
Documenter.jl <documenter@juliadocs.github.io>
2023-04-21 20:38:52 +0000

    build based on 8e5aa2c

---
+++ v0.3.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"reference/#Reference-Guide","page":"Reference","title":"Reference Guide","text":"","category":"section"},{"location":"reference/","page":"Reference","title":"Reference","text":"Model","category":"page"},{"location":"#MPSGE.jl","page":"Home","title":"MPSGE.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"TODO: Write documentation","category":"page"}]
+}
