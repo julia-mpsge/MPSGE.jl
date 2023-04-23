2ed51418918bcba0ca2bcfd3de722b95f9d9bdcc
EliLazarus <lazarus.eli@gmail.com>
2023-04-23 16:28:47 -0700

    minor lower case fix

--- examples/example3.jl
+++ examples/example3.jl
@@ -9,8 +9,8 @@ factors = [:l, :k]
 factor = DenseAxisArray(Float64[50 50; 20 30], goods, factors)
 supply = DenseAxisArray(Float64[100, 50], goods)
 outtax = add!(m, Parameter(:outtax, indices=(goods,), value=0.))
-intax = add!(m, Parameter(:intax, indices=(goods,), value=0.))
-endow = add!(m, Parameter(:endow, indices=(factors,), value=1.0))
+intax  = add!(m, Parameter(:intax,  indices=(goods,), value=0.))
+endow  = add!(m, Parameter(:endow,  indices=(factors,), value=1.0))
 
 Y = add!(m, Sector(:Y, indices=(goods,)))
 U = add!(m, Sector(:U))
@@ -22,13 +22,13 @@ PF = add!(m, Commodity(:PF, indices=(factors,)))
 RA = add!(m, Consumer(:RA, benchmark = 150.))
 
 for i in goods
-    @production(m, Y[i], 0, 1, [Output(PC[i], supply[i], [Tax(:(1 * $(outtax[i])), RA)])], [Input(PF[:l], factor[i,:l], [Tax(:(1 * $(Intax[i])), RA)]), Input(PF[:k], factor[i,:k])])
+    @production(m, Y[i], 0, 1, [Output(PC[i], supply[i], [Tax(:(1 * $(outtax[i])), RA)])], [Input(PF[:l], factor[i,:l], [Tax(:(1 * $(intax[i])), RA)]), Input(PF[:k], factor[i,:k])])
 end
 # @production(m, [i in goods], Y[i], 1, PC[i], supply[i],  [Input(PF[f], factor[i,f]) for f in factors])
 
 @production(m, U,0, 1, [Output(PU, 150)], [Input(PC[:x], 100), Input(PC[:y], 50)])
 
-@demand(m, RA, 1., [Demand(PU, 150)], [Endowment(PF[:l], :(70 * $(endow[:l]))), Endowment(PF[:k], :(80. * $(endow[:k])))])
+@demand(m, RA, 1., [Demand(PU, 150, [Tax(.1, RA)])], [Endowment(PF[:l], :(70 * $(endow[:l]))), Endowment(PF[:k], :(80. * $(endow[:k])))])
 
 set_fixed!(PC[:x], true) # Set sector x as the numeraire
 solve!(m, cumulative_iteration_limit=0)
@@ -56,6 +56,6 @@ set_value(outtax[:x], 0.1)
 solve!(m)
 
 set_value(outtax[:x], 0.0)
-set_value(intax[:l], 0.1)
+set_value(intax[:x], 0.1)
 solve!(m)
 algebraic_version(m)
