1ab1826ddd6456a3272fa9d8430aa1a5c3a672ae
odow <o.dowson@gmail.com>
2023-09-01 16:09:27 +1200

    Remove use of eval when building JuMP variables

--- src/build/build_variables.jl
+++ src/build/build_variables.jl
@@ -1,28 +1,46 @@
-function add_variable!(jm::JuMP.Model, name::Symbol, lower_bound::Union{Float64,Nothing}=nothing)    
+function add_variable!(jm::JuMP.Model, name::Symbol, lower_bound::Union{Float64,Nothing}=nothing)
     if lower_bound===nothing
         jm[name] = JuMP.@variable(jm, base_name=string(name))
-    else    
+    else
         jm[name] = JuMP.@variable(jm, base_name=string(name), lower_bound=lower_bound)
     end
 end
 
 function add_parameter_to_jump!(jm, parameter::ScalarParameter)
-    jmp_p = @eval(JuMP.@NLparameter($jm, $(parameter.name) == $(parameter.value)))
-    jm[parameter.name] = jmp_p
+    return jm[parameter.name] = JuMP.add_nonlinear_parameter(jm, parameter.value)
 end
 
 function add_parameter_to_jump!(jm, parameter::IndexedParameter)
     # We set the parameter value to 1.0 here, but in a later model building phase that gets replaced
     # with the actual values
-    jm[parameter.name] = @eval(JuMP.@NLparameter($jm, [$( ( :($(gensym())=$i) for i in parameter.indices)... )] == 1.0))
+    dim = length.(parameter.indices)
+    scalars = [JuMP.add_nonlinear_parameter(jm, 1.0) for _ in 1:prod(dim)]
+    return jm[parameter.name] = JuMP.Containers.DenseAxisArray(
+        reshape(scalars, dim),
+        parameter.indices...,
+    )
+end
+
+function add_indexed_variable!(
+    model::JuMP.Model,
+    data::Union{IndexedSector,IndexedCommodity,IndexedConsumer,IndexedAux},
+)
+    dim = length.(data.indices)
+    x = JuMP.@variable(model, [1:prod(dim)], lower_bound = 0)
+    for (i, index) in enumerate(Iterators.product(data.indices...))
+        JuMP.set_name(x[i], "$(data.name)[$index]")
+    end
+    output = JuMP.Containers.DenseAxisArray(reshape(x, dim), data.indices...)
+    model[data.name] = output
+    return output
 end
 
 function add_sector_to_jump!(jm, sector::ScalarSector)
     add_variable!(jm, sector.name, 0.)
 end
 
-function add_sector_to_jump!(jm, sector::IndexedSector)        
-        jm[sector.name] = @eval(JuMP.@variable($jm, [$( ( :($(gensym())=$i) for i in sector.indices)... )], base_name=string($(QuoteNode(sector.name))), lower_bound=0.))
+function add_sector_to_jump!(jm, sector::IndexedSector)
+    add_indexed_variable!(jm, sector)
 end
 
 function add_commodity_to_jump!(jm, commodity::ScalarCommodity)
@@ -30,7 +48,7 @@ function add_commodity_to_jump!(jm, commodity::ScalarCommodity)
 end
 
 function add_commodity_to_jump!(jm, commodity::IndexedCommodity)
-    jm[commodity.name] = @eval(JuMP.@variable($jm, [$( ( :($(gensym())=$i) for i in commodity.indices)... )], base_name=string($(QuoteNode(commodity.name))), lower_bound=0.))
+    add_indexed_variable!(jm, commodity)
 end
 
 function add_consumer_to_jump!(jm, consumer::ScalarConsumer)
@@ -38,7 +56,7 @@ function add_consumer_to_jump!(jm, consumer::ScalarConsumer)
 end
 
 function add_consumer_to_jump!(jm, consumer::IndexedConsumer)
-    jm[consumer.name] = @eval(JuMP.@variable($jm, [$( ( :($(gensym())=$i) for i in consumer.indices)... )], base_name=string($(QuoteNode(consumer.name))), lower_bound=0.))
+    add_indexed_variable!(jm, consumer)
 end
 
 function add_aux_to_jump!(jm, aux::ScalarAux)
@@ -46,7 +64,7 @@ function add_aux_to_jump!(jm, aux::ScalarAux)
 end
 
 function add_aux_to_jump!(jm, aux::IndexedAux)
-    jm[aux.name] = @eval(JuMP.@variable($jm, [$( ( :($(gensym())=$i) for i in aux.indices)... )], base_name=string($(QuoteNode(aux.name))), lower_bound=0.))
+    add_indexed_variable!(jm, aux)
 end
 
 function add_implicitvars!(m)
@@ -75,14 +93,12 @@ function build_variables!(m, jm)
 
     for p in m._parameters
         add_parameter_to_jump!(jm, p)
-        # jmp_p = @eval(JuMP.@NLparameter($jm, $(p.name) == $(p.value)))
-        # jm[p.name] = jmp_p
     end
 
     # Add all required variables
 
     for s in m._sectors
-        add_sector_to_jump!(jm, s)        
+        add_sector_to_jump!(jm, s)
     end
 
     for c in m._commodities
