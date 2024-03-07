3a71d0c63060fee1e7157451af84fd334db75d81
mitchphillipson <mitch.phillipson@gmail.com>
2024-03-07 11:05:45 -0600

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset "MPSGE_MP.jl" begin
 
     include("test_pruning.jl")
+    include("test_123.jl")
     include("test_Benchmark-Theta.jl")
     include("test_jpmge.jl")
     include("test_twobytwo_ces_functional.jl")
