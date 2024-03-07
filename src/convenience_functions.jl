5e8c66631fa0aeb0a981d34360f2ad0ff4ea838f
mitchphillipson <mitch.phillipson@gmail.com>
2024-03-07 11:04:55 -0600

    Reset lower bound after unfixing a variable

--- src/convenience_functions.jl
+++ src/convenience_functions.jl
@@ -2,5 +2,8 @@ value(X::MPSGEScalarVariable) = JuMP.value(get_variable(X))
 
 fix(X::MPSGEScalarVariable, value::Real) = JuMP.fix(get_variable(X), value; force=true)
 
-JuMP.unfix(X::MPSGEScalarVariable) = JuMP.unfix(get_variable(X))
+function JuMP.unfix(X::MPSGEScalarVariable) 
+    JuMP.unfix(get_variable(X))
+    JuMP.set_lower_bound(get_variable(X),0)
+end
 
