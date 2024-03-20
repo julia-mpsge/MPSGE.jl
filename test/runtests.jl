a5fdebeb2464976d39095f7486c98f2f8378265b
mitchphillipson <mitch.phillipson@gmail.com>
2024-03-20 09:39:33 -0500

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,13 @@ using Test
 
     include("test_pruning.jl")
     include("test_123.jl")
-    #include("test_Benchmark-Theta.jl")
-    #include("test_jpmge.jl")
-    #include("test_twobytwo_ces_functional.jl")
-    #include("test_twobytwo_functional.jl")
-    #include("test_twobytwo_indexed.jl")
-    #include("test_twobytwo_macro.jl")
-    #include("test_twobytwo_Price.jl")
-    #include("test_twobytwo_wTaxes.jl")
+    include("test_Benchmark-Theta.jl")
+    include("test_jpmge.jl")
+    include("test_twobytwo_ces_functional.jl")
+    include("test_twobytwo_functional.jl")
+    include("test_twobytwo_indexed.jl")
+    include("test_twobytwo_macro.jl")
+    include("test_twobytwo_Price.jl")
+    include("test_twobytwo_wTaxes.jl")
 
 end
