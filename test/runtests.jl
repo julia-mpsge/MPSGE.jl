3ed5a9f2141c0c658dfe895ab1d55bfe9c563ed0
mitchphillipson <mitch.phillipson@gmail.com>
2024-05-18 01:07:08 -0500

    Revert "Remove orientation_test (testing error)"
    This reverts commit 41869819d3afded1be24df791e3390bd215463b3.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,6 @@ using Test
 
 @testset "MPSGE_MP.jl" begin
 
-    
     include("test_pruning.jl")
     include("test_123.jl")
     include("test_Benchmark-Theta.jl")
@@ -15,7 +14,6 @@ using Test
     include("test_twobytwo_Price.jl")
     include("test_twobytwo_wTaxes.jl")
 
-
-    #include("test_orientation.jl")
+    include("test_orientation.jl")
 
 end
