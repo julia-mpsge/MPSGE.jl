97287bff362689a7831c390672f557421d40c71d
mitchphillipson <mitch.phillipson@gmail.com>
2024-06-17 09:15:52 -0500

    Simplifying types in arithmetic.jl

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -1,25 +1,25 @@
 const jump_types = Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}
 
 Base.:+(x::MPSGEScalarVariable, y::MPSGEScalarVariable) = get_variable(x)+get_variable(y) 
-Base.:+(x::MPSGEScalarVariable, y::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}) = get_variable(x)+y
-Base.:+(x::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}, y::MPSGEScalarVariable) = x+get_variable(y) 
+Base.:+(x::MPSGEScalarVariable, y::jump_types) = get_variable(x)+y
+Base.:+(x::jump_types, y::MPSGEScalarVariable) = x+get_variable(y) 
 
 Base.:-(x::MPSGEScalarVariable, y::MPSGEScalarVariable) = get_variable(x)-get_variable(y) 
-Base.:-(x::MPSGEScalarVariable, y::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}) = get_variable(x)-y
-Base.:-(x::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}, y::MPSGEScalarVariable) = x-get_variable(y) 
+Base.:-(x::MPSGEScalarVariable, y::jump_types) = get_variable(x)-y
+Base.:-(x::jump_types, y::MPSGEScalarVariable) = x-get_variable(y) 
 Base.:-(x::MPSGEScalarVariable) = -get_variable(x)
 
 Base.:*(x::MPSGEScalarVariable, y::MPSGEScalarVariable) = get_variable(x)*get_variable(y) 
-Base.:*(x::MPSGEScalarVariable, y::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}) = get_variable(x)*y
-Base.:*(x::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}, y::MPSGEScalarVariable) = x*get_variable(y) 
+Base.:*(x::MPSGEScalarVariable, y::jump_types) = get_variable(x)*y
+Base.:*(x::jump_types, y::MPSGEScalarVariable) = x*get_variable(y) 
 
 Base.:/(x::MPSGEScalarVariable, y::MPSGEScalarVariable) = get_variable(x)/get_variable(y) 
-Base.:/(x::MPSGEScalarVariable, y::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}) = get_variable(x)/y
-Base.:/(x::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}, y::MPSGEScalarVariable) = x/get_variable(y) 
+Base.:/(x::MPSGEScalarVariable, y::jump_types) = get_variable(x)/y
+Base.:/(x::jump_types, y::MPSGEScalarVariable) = x/get_variable(y) 
 
 Base.:^(x::MPSGEScalarVariable, y::MPSGEScalarVariable) = get_variable(x)^get_variable(y) 
-Base.:^(x::MPSGEScalarVariable, y::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}) = get_variable(x)^y
-Base.:^(x::Union{Real, JuMP.VariableRef, JuMP.AffExpr, JuMP.QuadExpr, JuMP.NonlinearExpr}, y::MPSGEScalarVariable) = x^get_variable(y) 
+Base.:^(x::MPSGEScalarVariable, y::jump_types) = get_variable(x)^y
+Base.:^(x::jump_types, y::MPSGEScalarVariable) = x^get_variable(y) 
 
 Base.zero(::Type{ScalarParameter}) = 0
 Base.zero(::Type{ScalarAuxiliary}) = 0
