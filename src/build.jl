e74c4a2af6ace7a33c2b44e594bcef4a53fea960
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-05-01 14:00:50 -0500

    Remove previous commit

--- src/build.jl
+++ src/build.jl
@@ -217,9 +217,9 @@ function solve!(m::AbstractMPSGEModel; kwargs...)
     if !haskey(JuMP.object_dictionary(jm), :z_p)
         build_constraints!(m)
 
-        for H in consumers(m)
-            set_start_value(H, consumer_income(H))
-        end
+        #for H in consumers(m)
+        #    set_start_value(H, consumer_income(H))
+        #end
     end
 
     #Set the default iteration limit to 10_000
