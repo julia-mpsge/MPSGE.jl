6ea1457549a2589606f36c492ef3a38d4f19d1e3
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-05-06 09:51:21 -0500

    Remove obsolete function

--- src/model.jl
+++ src/model.jl
@@ -6,26 +6,6 @@ function add_variable!(m::MPSGEModel, S::MPSGEScalarVariable; start = 1)
     jm[name(S)] = @variable(jm,base_name = string(name(S)),start=start, lower_bound = 0)
 end
 
-function add_variable!(m::MPSGEModel, S::MPSGEIndexedVariable; start = 1)
-
-    jm = jump_model(m)
-    index = S.index
-
-    dim = length.(index)
-    
-    x = JuMP.@variable(jm, [1:prod(dim)], lower_bound=0, start = start)
-
-    for (i, ind) in enumerate(Iterators.product(index...))
-        new_index = join(ind,",")
-        JuMP.set_name(x[i], "$(name(S))[$new_index]")
-    end
-
-    output = JuMP.Containers.DenseAxisArray(reshape(x, Tuple(dim)), index...)
-    jm[name(S)] = output
-    return output
-
-end
-
 function add_variable!(m::MPSGEModel, S::Auxiliary)
     add_variable!(m, S; start = 0)
 end
@@ -66,7 +46,6 @@ end
 
 
 function get_variable(S::MPSGEScalarVariable)
-
     jm = jump_model(S.model)
     return jm[name(S)]
 end
