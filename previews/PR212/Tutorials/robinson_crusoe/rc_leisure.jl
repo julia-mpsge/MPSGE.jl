835e4caa1b0ba1df3aeb70e9c7c0f709b66baa0c
Documenter.jl <documenter@juliadocs.github.io>
2025-06-16 14:35:18 +0000

    build based on e0ab663

---
+++ previews/PR212/Tutorials/robinson_crusoe/rc_leisure.jl
@@ -0,0 +1,47 @@
+# # One Consumer, One Producer, Two Goods with leisure
+#
+# ## Model Definition
+# 
+# After several years Crusoe realises he is spending all his time 
+# working and decides to allocate some of his time to leisure. 
+
+
+using MPSGE
+
+Lesiure = MPSGEModel()
+
+
+@sector(Lesiure,  rc_producer)
+
+@commodities(Lesiure,  begin
+    price_coconuts
+    price_fish
+    price_labor
+end)
+
+@consumer(Lesiure,  rc_consumer)
+
+
+
+@production(Lesiure,  rc_producer, [s=0, t=0], begin
+    @output(price_coconuts, 1, t)
+    @output(price_fish, 1, t)
+    @input(price_labor, 1, s)
+end)
+
+
+@demand(Lesiure,  rc_consumer, begin
+    @final_demand(price_coconuts, 1)
+    @final_demand(price_fish, 1)
+    @final_demand(price_labor, .5)
+    @endowment(price_labor, 1)
+end)
+
+
+fix(price_coconuts, 1)
+
+
+solve!(Lesiure)
+
+generate_report(Lesiure)
+
