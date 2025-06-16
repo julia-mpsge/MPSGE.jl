72d14b748965debc931a268be0c4bd626d8ec363
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-06-16 09:40:34 -0500

    Adding docstrings to demand macro

--- src/macros/@demand.jl
+++ src/macros/@demand.jl
@@ -53,12 +53,54 @@ end
 
 
 
+"""
+    @final_demand(commodity, quantity, nest, kwargs...)
 
+This macro is soley used within a [`@demand`](@ref) block to define the final demands 
+for a consumer. 
+
+## Required Arguments
+
+- `commodity` - A commodity. This can either be a scalar `C` or indexed `C[i=R]`. 
+- `quantity` - Either a number or expression. 
+
+## Keyword Arguments
+
+- `reference_price=1` Set the reference price of a commodity.
+
+## Examples
+
+```julia
+@final_demand(C, 1)
+@final_demand(C[i=R], 1)
+@final_demand(C[i=R], quantity[i])
+@final_demand(C[i=R], quantity[i], reference_price = price[i])
+```
+"""
 macro final_demand(input_args...)
     error_fn = Containers.build_error_fn("final_demand", input_args, __source__)
     return parse_demand_netput(error_fn, input_args, ScalarFinalDemand, __FINAL_DEMAND_KWARGS__)
 end
 
+"""
+    @endowment(commodity, quantity, nest, kwargs...)
+
+This macro is soley used within a [`@demand`](@ref) block to define the endowments 
+for a consumer. 
+
+## Required Arguments
+
+- `commodity` - A commodity. This can either be a scalar `C` or indexed `C[i=R]`. 
+- `quantity` - Either a number or expression. 
+
+## Examples
+
+```julia
+@endowment(C, 1)
+@endowment(C[i=R], 1)
+@endowment(C[i=R], quantity[i])
+```
+"""
 macro endowment(input_args...)
     error_fn = Containers.build_error_fn("endowment", input_args, __source__)
     return parse_demand_netput(error_fn, input_args, ScalarEndowment, __ENDOWMENT_KWARGS__)
@@ -109,7 +151,92 @@ struct PreDemand
 end
 
 
+"""
+    @demand(model, comsumer, demand_netputs; elasticity = 1)
 
+Define a demand for a `consumer` in the `model` with given `demand_netputs` where 
+the `final_demands` have an elasticity of substitution `elasticity`.
+
+## consumer
+
+The `consumer` can take the following forms:
+
+- `X`, if `X` is a scalar consumer.
+- `X[i=I,j=J,...]` if the consumer is indexed. Indexing
+- `X[i=I, :j]` if `:j` is an element of the second index of `X`.
+- `X[i=I, j]` if `j` is defined outside the demand block, for example in a `for` loop.
+
+For an indexed consumer it is __required__ that an index is provided when iterating over an array. For example, 
+
+```julia
+julia> I = [:a,:b]
+
+julia> @consumer(M, X[i=I])
+
+julia> demand(M, X[I], ...) # This is not allowed and will error.
+
+julia> demand(M, X[i=I], ...) # This is required behavior.
+
+julia>  for i in I
+            demand(M, X[i], ...) # This is allowed.
+        end
+```
+
+## demand_netputs
+
+A demand netput is either a [`@final_demand`](@ref) or an [`@endowment`](@ref). The netputs get wrapped in a `begin ... end` block and each netput must be on its own line. For examples creating netputs, see the netput documentation and the examples below.
+
+Netputs can use indices initialized in the `consumer`, but can not use them as a new index. For example, if we have `X[i=I]` in the demand block, we can use
+`@final_demand(PX[i],...)` but not `@final_demand(PX[i=I],...)`. The latter will error.
+
+## Examples
+
+We demonstrate three ways to define a demand block. 
+
+```julia
+julia> M = MPSGEModel();
+
+julia> I = [:a,:b]
+
+julia> @parameters(M, begin
+            elas[i=I,j=J], 4
+            T[i=I], .1
+        end)
+
+julia> @commodities(M, begin
+            PX
+            PY
+            PL[i=I]
+            PK
+        end);
+
+julia> @consumers(M, begin
+            X
+            Y[i=I, j=J]
+            Z[i=I]
+        end);
+
+julia> @demand(M, X, begin
+            @final_demand(PX, 10, t)
+            @endowment(PL, 5, s)
+            @endowment(PK, 5, s)
+        end);
+
+julia> @demand(M, Y[i=I,j=J], [s=2, t=1, begin
+            @final_demand(PY, 15, t, reference_price = 2)
+            @endowment(PX, 3, s)
+            @endowment(PL[ii=I], 4, va[ii])
+            @endowment(PK, 6, va[i])
+        end, elasticity = 2);
+
+julia> for i in I
+            @demand(M, Z[i], begin
+                @final_demand(PK, 20, t)
+                @endowment(PL[i], 2, s)
+            end)
+        end
+```
+"""
 macro demand(input_args...)
     error_fn = Containers.build_error_fn("production", input_args, __source__)
     args,kwargs = Containers.parse_macro_arguments(
