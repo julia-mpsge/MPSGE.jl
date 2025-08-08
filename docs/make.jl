52201b3fe985478a31fb953b3a9ef78d4da964b9
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-08-08 07:21:15 -0500

    Convert example to literate

--- docs/make.jl
+++ docs/make.jl
@@ -8,9 +8,17 @@ DocMeta.setdocmeta!(MPSGE, :DocTestSetup, :(using MPSGE); recursive=true)
 const _PAGES = [
     "Introduction" => ["index.md"],
     "Tutorials" => [
-        "Getting Started" => ["Tutorials/getting_started/introduction.md", "Tutorials/getting_started/first_example.md"],
-        "Intermediate Examples" => ["Tutorials/intermediate_examples/M22.md"],
-        "Robinson Crusoe" => ["Tutorials/robinson_crusoe/introduction.md","Tutorials/robinson_crusoe/basic_rc.md"],
+        "Getting Started" => [
+            "Tutorials/getting_started/introduction.md", 
+            "Tutorials/getting_started/first_example.md"
+        ],
+        "Intermediate Examples" => [
+            "Tutorials/intermediate_examples/m22.md"
+            ],
+        "Robinson Crusoe" => [
+            "Tutorials/robinson_crusoe/introduction.md",
+            "Tutorials/robinson_crusoe/basic_rc.md"
+            ],
     ],
     #"Cookbook" => [],
     "Mathematical Background" => ["how_it_works.md"], 
@@ -18,19 +26,31 @@ const _PAGES = [
 ]
 
 
+literate_files = Dict(
+    "basic_rc" => ( 
+        input = "src/Tutorials/robinson_crusoe/basic_rc.jl",
+        output = "src/Tutorials/robinson_crusoe/"
+    ),
+    "first_example" => ( 
+        input = "src/Tutorials/getting_started/first_example.jl",
+        output = "src/Tutorials/getting_started/"
+    ),
+    "m22" => ( 
+        input = "src/Tutorials/intermediate_examples/M22.jl",
+        output = "src/Tutorials/intermediate_examples/"
+    )
+)
 
 
-EXAMPLE = joinpath(@__DIR__, "src/Tutorials/robinson_crusoe/basic_rc.jl")
-OUTPUT = joinpath(@__DIR__,"src/Tutorials/robinson_crusoe/")
-Literate.markdown(EXAMPLE, 
-                  OUTPUT;
-                  name = "basic_rc")
+for (name, paths) in literate_files
+    EXAMPLE = joinpath(@__DIR__, paths.input)
+    OUTPUT = joinpath(@__DIR__, paths.output)
+    Literate.markdown(EXAMPLE, 
+                      OUTPUT;
+                      name = name)
+end
 
-EXAMPLE = joinpath(@__DIR__, "src/Tutorials/getting_started/first_example.jl")
-OUTPUT = joinpath(@__DIR__,"src/Tutorials/getting_started/")
-Literate.markdown(EXAMPLE, 
-                  OUTPUT;
-                  name = "first_example")
+
 
 makedocs(;
     modules=[MPSGE],
