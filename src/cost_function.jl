a74b0c03a3149b6f2f1a3be801b5afb7bec842eb
Mitch Phillipson <mitch.phillipson@gmail.com>
2026-02-25 08:27:09 -0600

    Add unit cost function

--- src/cost_function.jl
+++ src/cost_function.jl
@@ -1,19 +1,46 @@
-function cobb_douglas(N::Node; virtual = :full, cf = cost_function, sector = nothing)
-    sign = netput_sign(N)
-    return prod(cf(child; virtual = virtual, sector = sector)^(quantity(child)/quantity(N)) for child in children(N); init=1)
+function cobb_douglas(N::MPSGE.Node; depth = -1)
+    sign = MPSGE.netput_sign(N)
+    return prod(unit_cost_function(child; depth = depth)^(quantity(child)/quantity(N)) for child in MPSGE.children(N); init=1)
 end
 
-function CES(N::Node; virtual = :full,  cf = cost_function, sector = nothing)
-    sign = netput_sign(N)
-    return sum(quantity(child)/quantity(N) * cf(child; virtual = virtual, sector = sector)^(1+sign*elasticity(N)) for child in children(N); init=0) ^ (1/(1+sign*elasticity(N)))
+function CES(N::MPSGE.Node; depth = -1)
+    sign = MPSGE.netput_sign(N)
+    return sum(quantity(child)/quantity(N) * unit_cost_function(child; depth = depth)^(1+sign*elasticity(N)) for child in MPSGE.children(N); init=0) ^ (1/(1+sign*elasticity(N)))
 end
 
 
+function unit_cost_function(N::MPSGE.Netput; depth = -1)
+    C = commodity(N)
+    sign = netput_sign(N)
+    rp = reference_price(N)
+    return C*(1-sign*sum(tax(t) for t∈taxes(N);init = 0))/rp
+end
 
+function unit_cost_function(N::MPSGE.Node; depth = -1)
+    if depth == 0
+        return N.cost_function_virtual
+    end
 
-#cost_function(N::Node; virtual = false) = !virtual ? N.cost_function : N.cost_function_virtual
+    sign = MPSGE.netput_sign(N)
+    if !(isa(MPSGE.elasticity(N), Real))
+
+        jm = jump_model(model(N))
+
+        #This must be an explicit expression, otherwise it's evaluated now. 
+        cost_function = @expression(jm, ifelse(
+                    MPSGE.elasticity(N) * sign == -1,
+                    cobb_douglas(N; depth = depth-1), 
+                    CES(N; depth = depth-1)
+                ))
+    elseif MPSGE.elasticity(N)*sign == -1 #Cobb-Douglas is only on demand side with σ=1
+        cost_function = cobb_douglas(N; depth = depth-1)
+    else
+        cost_function = CES(N; depth = depth-1)
+    end
+end
 
 function cost_function(N::MPSGE.Netput; virtual = false, sector = nothing)
+    return 
     C = commodity(N)
     sign = MPSGE.netput_sign(N)
     rp = MPSGE.reference_price(N)
@@ -109,25 +136,33 @@ revenue_function(S::ScalarSector; virtual = false) = revenue_function(production
 
 
 function build_cost_function(tree::Netput; virtual = :full, sector = nothing)
-    return cost_function(tree)
+    return unit_cost_function(tree)
 end
 
 function build_cost_function(N::Node; virtual = :full, sector = nothing)
 
     # If the cost function exists, return it
-    if !isnothing(N.cost_function_virtual)
-        return MPSGE.cost_function(N, virtual = :virtual)
-    end
+    #if !isnothing(N.cost_function_virtual)
+    #    return unit_cost_function(N; depth = 0)
+    #end
 
-    cost_function = MPSGE.cost_function(N; virtual = :partial, cf = MPSGE.build_cost_function, sector = sector)
+    #return 0
+    #cost_function = MPSGE.cost_function(N; virtual = :partial, cf = MPSGE.build_cost_function, sector = sector)
 
-    if isnothing(N.cost_function_virtual)
-        jm = jump_model(model(N))
-        name = string("cf(", sector, ", :", MPSGE.name(N), ")")
-        N.cost_function_virtual = @variable(jm, start = value(start_value, cost_function), base_name = name) 
-        N.cost_function = cost_function
-        @constraint(jm, N.cost_function_virtual - cost_function ⟂ N.cost_function_virtual)
-    end
+    ucf = unit_cost_function(N; depth = 1)
+
+    jm = jump_model(model(N))
+    @constraint(jm, N.cost_function_virtual - ucf ⟂ N.cost_function_virtual)
+
+    build_cost_function.(children(N))
+
+    #if isnothing(N.cost_function_virtual)
+        #jm = jump_model(model(N))
+        #name = string("cf(", sector, ", :", MPSGE.name(N), ")")
+        #N.cost_function_virtual = @variable(jm, start = value(start_value, cost_function), base_name = name) 
+        #N.cost_function = cost_function
+        #@constraint(jm, N.cost_function_virtual - cost_function ⟂ N.cost_function_virtual)
+    #end
 
     return N.cost_function_virtual
 end
