61b0f1233ecfe8f1fad3944794b82650f3522829
Mitch Phillipson <mitch.phillipson@gmail.com>
2026-03-06 08:07:44 -0600

    Update indexed test

--- test/test_start_values.jl
+++ test/test_start_values.jl
@@ -13,7 +13,7 @@
 
     @consumer(M, C)
 
-    @auxiliary(M, A)
+    @auxiliary(M, A, start = 0.5)
 
     @production(M, X, [s=0, t=0], begin
         @output(PX, .5*A, t, reference_price = 2)
@@ -43,40 +43,62 @@ end
 
 @testitem "Updating Start Values in Indexed Model" begin
 
+    I = 1:5
+
     M = MPSGEModel()
 
     @sectors(M, begin
-        X[i=1:2]
+        X[i=I]
+        Y[i=I]
     end)
 
     @commodities(M, begin
-        PX[i=1:2]
-        PY[i=1:2]
+        PX[i=I]
+        PY[i=I]
+        PL[i=I]
     end)
 
-    @consumer(M, C)
+    @consumer(M, CONS)
 
-    @production(M, X[i=1:2], [s=0, t=0], begin
-        @output(PX[i], .5, t, reference_price = 2)
-        @input(PY[i], 1, s, reference_price = 1)
-    end)
 
-    @demand(M, C, begin
-        @final_demand(PX[i=1:2], 1)
-        @endowment(PY[i=1:2], 2)
+    @production(M, X[i=I], [s=1,t=0, va=>s=2], begin
+        @output(PX[i], 10, t)
+        @input(PY[j=I], 2, s)
+        @input(PL[j=I], 1, va)
     end)
 
 
-    solve!(M; cumulative_iteration_limit=0)
+    @production(M, Y[i=I], [s=1,t=0, va=>s=2], begin
+        @output(PY[i], 10, t)
+        @input(PX[j=I], 2, s)
+        @input(PL[j=I], 1, va)
+    end)
 
+    @demand(M, CONS, begin
+        @final_demand(PX[i=I], 100)
+        @final_demand(PY[i=I], 100)
+        @endowment(PL[i=I], 100)
+    end)
 
+    fix(PL[1], 1)
+
+    # Not balanced
+    solve!(M, cumulative_iteration_limit=0)
     @test !MPSGE.is_solved_and_feasible(M)
 
-    set_start_value.(X, 2)
-    set_start_value.(PX, 2)
+    # Solve
+    solve!(M)
+    @test MPSGE.is_solved_and_feasible(M)
 
-    solve!(M; cumulative_iteration_limit=0)
+    # Update start values
+    set_start_value.(X, value.(X))
+    set_start_value.(Y, value.(Y))
+    set_start_value.(PX, value.(PX))
+    set_start_value.(PY, value.(PY))
+    set_start_value.(PL, value.(PL))
+    set_start_value(CONS, value(CONS))
 
+    solve!(M, cumulative_iteration_limit=0)
     @test MPSGE.is_solved_and_feasible(M)
 
 
