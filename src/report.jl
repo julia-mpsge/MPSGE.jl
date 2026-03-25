f6ffc405551fc5d68019bb18e859ff9723662047
Mitch Phillipson <mitch.phillipson@gmail.com>
2026-03-25 09:30:43 -0500

    Update report functions

--- src/report.jl
+++ src/report.jl
@@ -22,6 +22,7 @@ end
 
 """
     generate_report(M::MPSGEModel)
+    generate_report(variables::Union{MPSGEIndexedVariable, MPSGEScalarVariable}...)
 
 Returns a dataframe with three columns:
 
@@ -32,14 +33,30 @@ Returns a dataframe with three columns:
 The product of the value and the margin should be zero, if not the model has a 
 specification error.
 
-This function is useful for debugging models that fail a calibrated benchmark. 
-If the model fails the benchmark, look for non-zero margins in this report, as 
-they will reveal the error.
+## Arguments
+
+There are two methods for this function:
+
+    1. `M::MPSGEModel`: This will report on all sectors, commodities, consumers, and auxiliaries in the model.  
+    2. `variables::Union{MPSGEIndexedVariable, MPSGEScalarVariable}...`: This will report on the specified variables.
+
+## Example
+
+Let `M` be an MPSGEModel with sectors `X` and `EX`. The following code will report
+on all variables in the model:
+
+```julia
+generate_report(M)
+```
+
+The following code will report only on the variables `X` and `EX`:
+
+```julia
+generate_report(X, EX)
+```
 """
 function generate_report(M::MPSGEModel)
-
     out = []
-
 
     vars_to_report = [
         production_sectors(M);
@@ -56,6 +73,30 @@ function generate_report(M::MPSGEModel)
 
 end
 
+function generate_report(variable::MPSGEIndexedVariable)
+    vars_to_report = extract_scalars(variable) |> vec
+
+    return DataFrame(
+        OrderedDict(
+            :var => vars_to_report,
+            :value => value.(vars_to_report),
+            :margin => value.(constraint.(vars_to_report))
+        ))
+end
+
+function generate_report(variable::MPSGEScalarVariable)
+    return DataFrame(var = variable, value = value(variable), margin = value(constraint(variable)))
+end
+
+function generate_report(variables::Union{MPSGEIndexedVariable, MPSGEScalarVariable}...)
+    out = []
+
+    for variable in variables
+        push!(out, generate_report(variable))
+    end
+
+    return vcat(out...)
+end
 
 
 
@@ -78,6 +119,7 @@ end
     report(X::MPSGEVariable)
 
 Return a dataframe with the following columns:
+
     - `var`: The name of the variable
     - `value`: The value of the variable
     - `margin`: The value of the constraint for the variable
