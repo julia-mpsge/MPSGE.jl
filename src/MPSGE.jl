f1fa8d104766ea7210bd0f7e57d060b599bc9f5c
Mitch Phillipson <mitch.phillipson@gmail.com>
2026-03-25 08:36:46 -0500

    Export `report`

--- src/MPSGE.jl
+++ src/MPSGE.jl
@@ -73,7 +73,7 @@ export  compensated_demand, tau, demand, endowment,
         zero_profit, market_clearance, income_balance
 
 #Reporting
-export generate_report, solve!
+export generate_report, solve!, report
 
 #Convenience
 export  value, fix, unfix, is_fixed, 
